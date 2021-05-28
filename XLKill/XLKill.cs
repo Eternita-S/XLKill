dd2eafb02de69f9660c86fb42be5e0097318baeb
Eternita <lysithea.star@gmail.com>
2021-05-28 09:19:37 +0300

    Add project files.

---
+++ XLKill/XLKill.cs
@@ -0,0 +1,33 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XLKill
+{
+    class XLKill : IDalamudPlugin
+    {
+        internal DalamudPluginInterface pi;
+
+        public string Name => "XLKill";
+
+        public void Dispose()
+        {
+            pi.CommandManager.RemoveHandler("/xlkill");
+            pi.Dispose();
+        }
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            pi = pluginInterface;
+            pi.CommandManager.AddHandler("/xlkill", new CommandInfo(delegate 
+            {
+                Process.GetCurrentProcess().Kill();
+            }));
+        }
+    }
+}
